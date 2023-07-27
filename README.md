1cf375c60743f65906e396172be962b96301802c
Semen-prog <s.kukharenko@sch-int.ru>
2023-07-27 13:40:26 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# rpi-project-mars
-Программа Raspberry Pi 3 для проекта МАРС
+# [Проект МАРС](http://176.120.8.20). Код аппаратной части
+
+Программа Raspberry Pi 3 для [проекта МАРС](http://176.120.8.20)
+
+### Содержание:
+ - **rpi\_handler.py** - основной исполняемый файл
+ - **rpi_image_cache** - папка для сохранения распознаваемых изображений
+ - **conf.ini** - конфигурационный файл
+
+ ### Использование:
+
+ 1. #### Клонируйте репозиторий
+    
+        $ git clone https://github.com/MersennexTwister/rpi-project-mars
+
+ 2. #### Заполните конфигурационный файл conf.ini в формате:
+
+        [user-data]
+        login='_ваш логин_'
+        password = '_ваш пароль_'
+
+        [system-data]
+        root='/путь/до/репозитория/rpi-project-mars'
+        url='http://176.120.8.20/'
+
+    Важно, чтобы в конце root не было слеша.
+
+ 3. #### Запустите rpi_handler.py
+
+        $ python3 rpi_handler.py 
---
+++ rpi_handler.py
@@ -0,0 +1,96 @@
+import requests as rqs
+import os
+import RPi.GPIO as GPIO
+import time
+import picamera
+import configparser
+from bs4 import BeautifulSoup
+
+BUTTON_P = 8
+BUTTON_M = 10
+BUTTON_STOP = 12
+DELAY_SEC = 0.05
+
+prev_p, cur_p = False, False
+prev_m, cur_m = False, False
+
+config = configparser.ConfigParser()
+config.read('conf.ini')
+
+URL = config["system-data"]['url']
+LOGIN = config["user-data"]["login"]
+PASSWORD = config["user-data"]["password"]
+APP_ROOT = config["system-data"]["root"]
+
+
+camera = picamera.PiCamera()
+
+def setup():
+    camera.resolution = (480, 320)
+
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(BUTTON_P, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(BUTTON_M, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(BUTTON_STOP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def image_request(mark):
+    camera.resolution = (2160, 1440)
+
+    r = rqs.post(URL + 'login', data={'login': LOGIN, 'password': PASSWORD})
+    
+    camera.capture(APP_ROOT + '/rpi_image_cache/cached.jpg')
+    
+    img = open(APP_ROOT + '/rpi_image_cache/cached.jpg', 'rb')
+
+    files = {'photo': img}
+
+    r = rqs.post(URL + 'lk/put_mark', files=files, data={'mark': mark})
+
+    img.close()
+    os.remove(APP_ROOT +'/rpi_image_cache/' + 'cached.jpg')
+    camera.resolution = (480, 320)
+
+    s = r.text
+    soup = BeautifulSoup(s, 'html.parser')
+
+    data = soup.findAll('b')
+    if len(data) == 0:
+        print('Система не смогла распознать ученика в камере')
+    else:
+        print(f'Имя ученика: {data[0].text}')
+
+
+
+if __name__ == '__main__':
+    setup()
+
+    camera.start_preview()
+    
+    while True:
+        cur_p = GPIO.input(BUTTON_P)
+        cur_m = GPIO.input(BUTTON_M)
+
+        if GPIO.input(BUTTON_STOP):
+            exit()
+
+        if not prev_p and cur_p:
+            time.sleep(DELAY_SEC)
+            cur_p = GPIO.input(BUTTON_P)
+            if not prev_p and cur_p:
+                camera.stop_preview()
+                print("Button + pressed")
+                image_request('+')
+                camera.start_preview()
+            
+        elif not prev_m and cur_m:
+            time.sleep(DELAY_SEC)
+            cur_m = GPIO.input(BUTTON_M)
+            if not prev_m and cur_m:
+                camera.stop_preview()
+                print("Button - pressed")
+                image_request('-') 
+                camera.start_preview()
+
+        prev_p = cur_p
+        prev_m = cur_m
